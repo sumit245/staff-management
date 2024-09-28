815eee836d782967c54f35ccabd893e979c3aa51
Rohit <rgupta6205@gmail.com>
2024-09-28 11:45:13 +0530

    changes in code

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import DashboardScreen from "./screens/DashboardScreen";
 import LoginScreen from "./screens/LoginScreen";
 import CardTask from "./components/CardTask";
+import ProjectOverview from "./screens/ProjectOverview";
 
 const App = () =>
   // <DashboardScreen />;
-  <CardTask/>
+  <ProjectOverview />;
+  // <CardTask/>
 
 export default App;
