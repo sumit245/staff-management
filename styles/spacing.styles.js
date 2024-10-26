f46d256d82fc9093c4969f19882ced7e3eebbe5e
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-10-26 23:20:47 +0530

    Added pr,pt,pb

--- styles/spacing.styles.js
+++ styles/spacing.styles.js
@@ -1,189 +1,235 @@
 import { StyleSheet } from "react-native"
 
 export const spacing = StyleSheet.create({
-    // Styles for horizontal padding
-    ph1: {
-        paddingHorizontal: 4,
-    },
-    ph2: {
-        paddingHorizontal: 8,
-    },
-    ph3: {
-        paddingHorizontal: 12,
-    },
-    ph4: {
-        paddingHorizontal: 16,
-    },
-    ph5: {
-        paddingHorizontal: 20,
-    },
+  // Styles for horizontal padding
+  ph1: {
+    paddingHorizontal: 4,
+  },
+  ph2: {
+    paddingHorizontal: 8,
+  },
+  ph3: {
+    paddingHorizontal: 12,
+  },
+  ph4: {
+    paddingHorizontal: 16,
+  },
+  ph5: {
+    paddingHorizontal: 20,
+  },
 
-    // Styles for padding vertical
-    pv1: {
-        paddingVertical: 4,
-    },
-    pv2: {
-        paddingVertical: 8,
-    },
-    pv3: {
-        paddingVertical: 12,
-    },
-    pv4: {
-        paddingVertical: 16,
-    },
-    pv5: {
-        paddingVertical: 20,
-    },
+  // Styles for padding vertical
+  pv1: {
+    paddingVertical: 4,
+  },
+  pv2: {
+    paddingVertical: 8,
+  },
+  pv3: {
+    paddingVertical: 12,
+  },
+  pv4: {
+    paddingVertical: 16,
+  },
+  pv5: {
+    paddingVertical: 20,
+  },
 
-    //Styles for all padding
-    p1: {
-        padding: 4,
-    },
-    p2: {
-        padding: 8,
-    },
-    p3: {
-        padding: 12,
-    },
-    p4: {
-        padding: 16,
-    },
-    p5: {
-        padding: 20,
-    },
+  //Styles for all padding
+  p1: {
+    padding: 4,
+  },
+  p2: {
+    padding: 8,
+  },
+  p3: {
+    padding: 12,
+  },
+  p4: {
+    padding: 16,
+  },
+  p5: {
+    padding: 20,
+  },
 
-    //Styles for all padding
-    pl1: {
-        paddingLeft: 4,
-    },
-    pl2: {
-        paddingLeft: 8,
-    },
-    pl3: {
-        paddingLeft: 12,
-    },
-    pl4: {
-        paddingLeft: 16,
-    },
-    pl5: {
-        paddingLeft: 20,
-    },
-    // TODO:pr,pt,pb
+  //Styles for all padding
+  pl1: {
+    paddingLeft: 4,
+  },
+  pl2: {
+    paddingLeft: 8,
+  },
+  pl3: {
+    paddingLeft: 12,
+  },
+  pl4: {
+    paddingLeft: 16,
+  },
+  pl5: {
+    paddingLeft: 20,
+  },
+  // TODO:pr,pt,pb
 
-    // Styles for horizontal margin
-    mh1: {
-        marginHorizontal: 4,
+  pr1: {
+    paddingRight: 4,
+  },
+  pr2: {
+    paddingRight: 8,
+  },
+  pr3: {
+    paddingRight: 12,
+  },
+  pr4: {
+    paddingRight: 16,
+  },
+  pr5: {
+    paddingRight: 20,
+  },
+  pt1: {
+    paddingTop: 4,
+  },
+  pt2: {
+    paddingTop: 8,
+  },
+  pt3: {
+    paddingTop: 12,
+  },
+  pt4: {
+    paddingTop: 16,
+  },
+  pt5: {
+    paddingTop: 20,
+  },
+  pb1: {
+    paddingBottom: 4,
+  },
+  pb2: {
+    paddingBottom: 8,
+  },
+  pb3: {
+    paddingBottom: 12,
+  },
+  pb4: {
+    paddingBottom: 16,
+  },
+  pb5: {
+    paddingBottom: 20,
     },
-    mh2: {
-        marginHorizontal: 8,
-    },
-    mh3: {
-        marginHorizontal: 12,
-    },
-    mh4: {
-        marginHorizontal: 16,
-    },
-    mh5: {
-        marginHorizontal: 20,
-    },
+  //DONE
 
-    // Styles for vertical margin
-    mv1: {
-        marginVertical: 4,
-    },
-    mv2: {
-        marginVertical: 8,
-    },
-    mv3: {
-        marginVertical: 12,
-    },
-    mv4: {
-        marginVertical: 16,
-    },
-    mv5: {
-        marginVertical: 20,
-    },
+  // Styles for horizontal margin
+  mh1: {
+    marginHorizontal: 4,
+  },
+  mh2: {
+    marginHorizontal: 8,
+  },
+  mh3: {
+    marginHorizontal: 12,
+  },
+  mh4: {
+    marginHorizontal: 16,
+  },
+  mh5: {
+    marginHorizontal: 20,
+  },
 
-    // Styles for vertical margin
-    mt1: {
-        marginTop: 4,
-    },
-    mt2: {
-        marginTop: 8,
-    },
-    mt3: {
-        marginTop: 12,
-    },
-    mt4: {
-        marginTop: 16,
-    },
-    mt5: {
-        marginTop: 20,
-    },
+  // Styles for vertical margin
+  mv1: {
+    marginVertical: 4,
+  },
+  mv2: {
+    marginVertical: 8,
+  },
+  mv3: {
+    marginVertical: 12,
+  },
+  mv4: {
+    marginVertical: 16,
+  },
+  mv5: {
+    marginVertical: 20,
+  },
 
-    mb1: {
-        marginBottom: 4,
-    },
-    mb2: {
-        marginBottom: 8,
-    },
-    mb3: {
-        marginBottom: 12,
-    },
-    mb4: {
-        marginBottom: 16,
-    },
-    mb5: {
-        marginBottom: 20,
-    },
+  // Styles for vertical margin
+  mt1: {
+    marginTop: 4,
+  },
+  mt2: {
+    marginTop: 8,
+  },
+  mt3: {
+    marginTop: 12,
+  },
+  mt4: {
+    marginTop: 16,
+  },
+  mt5: {
+    marginTop: 20,
+  },
 
-    ml1: {
-        marginLeft: 4,
-    },
-    ml2: {
-        marginLeft: 8,
-    },
-    ml3: {
-        marginLeft: 12,
-    },
-    ml4: {
-        marginLeft: 16,
-    },
-    ml5: {
-        marginLeft: 20,
-    },
+  mb1: {
+    marginBottom: 4,
+  },
+  mb2: {
+    marginBottom: 8,
+  },
+  mb3: {
+    marginBottom: 12,
+  },
+  mb4: {
+    marginBottom: 16,
+  },
+  mb5: {
+    marginBottom: 20,
+  },
 
-    mr1: {
-        marginRight: 4,
-    },
-    mr2: {
-        marginRight: 8,
-    },
-    mr3: {
-        marginRight: 12,
-    },
-    mr4: {
-        marginRight: 16,
-    },
-    mr5: {
-        marginRight: 20,
-    },
-    // TODO:m
+  ml1: {
+    marginLeft: 4,
+  },
+  ml2: {
+    marginLeft: 8,
+  },
+  ml3: {
+    marginLeft: 12,
+  },
+  ml4: {
+    marginLeft: 16,
+  },
+  ml5: {
+    marginLeft: 20,
+  },
 
-    br1: {
-        borderRadius: 6
-    },
-    br2: {
-        borderRadius: 12
-    },
-    br3: {
-        borderRadius: 18
-    },
-    br4: {
-        borderRadius: 24
-    },
-    br5: {
-        borderRadius: 30
-    }
+  mr1: {
+    marginRight: 4,
+  },
+  mr2: {
+    marginRight: 8,
+  },
+  mr3: {
+    marginRight: 12,
+  },
+  mr4: {
+    marginRight: 16,
+  },
+  mr5: {
+    marginRight: 20,
+  },
+  // TODO:m
 
-})
+  br1: {
+    borderRadius: 6,
+  },
+  br2: {
+    borderRadius: 12,
+  },
+  br3: {
+    borderRadius: 18,
+  },
+  br4: {
+    borderRadius: 24,
+  },
+  br5: {
+    borderRadius: 30,
+  },
+});
