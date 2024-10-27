f65e293fb25e39b8218ab1ac6dad2a5320a601f8
Rohit <rgupta6205@gmail.com>
2024-10-27 12:25:16 +0530

    style reduce

--- styles/typography.styles.js
+++ styles/typography.styles.js
@@ -1,42 +1,50 @@
-import { INFO_COLOR, PRIMARY_COLOR, PRIMARY_COLOR_TRANSPARENT, SECONDARY_COLOR, LIGHT, DANGER_COLOR, DARK } from "./constant";
+import {
+  INFO_COLOR,
+  PRIMARY_COLOR,
+  PRIMARY_COLOR_TRANSPARENT,
+  SECONDARY_COLOR,
+  LIGHT,
+  DANGER_COLOR,
+  DARK,
+} from "./constant";
 
 import { StyleSheet } from "react-native";
 
 export const typography = StyleSheet.create({
-    textPrimary: {
-        color: PRIMARY_COLOR
-    },
-    textSecondary: {
-        color: SECONDARY_COLOR,
-    },
-    textLight: {
-        color: LIGHT,
-    },
-    textDark: {
-        color: DARK,
-    },
-    textDanger: {
-        color: DANGER_COLOR,
-    },
-    textInfo: {
-        color: INFO_COLOR
-    },
-    textBold: {
-        fontWeight: 'bold'
-    },
+  textPrimary: {
+    color: PRIMARY_COLOR,
+  },
+  textSecondary: {
+    color: SECONDARY_COLOR,
+  },
+  textLight: {
+    color: LIGHT,
+  },
+  textDark: {
+    color: DARK,
+  },
+  textDanger: {
+    color: DANGER_COLOR,
+  },
+  textInfo: {
+    color: INFO_COLOR,
+  },
 
-    font12: {
-        fontSize: 12
-    },
+  textBold: {
+    fontWeight: "bold",
+  },
 
-    font14: {
-        fontSize: 14
-    },
-    font16: {
-        fontSize: 16
-    },
-    font20: {
-        fontSize: 20
-    }
+  font12: {
+    fontSize: 12,
+  },
 
-})
+  font14: {
+    fontSize: 14,
+  },
+  font16: {
+    fontSize: 16,
+  },
+  font20: {
+    fontSize: 20,
+  },
+});
