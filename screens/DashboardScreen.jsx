ba2eccacc3e40dbaa66ee293bcc5379e351259eb
Mihir Kumar Mishra <mkm20170@gmail.com>
2024-10-27 17:50:11 +0530

    Worked on Dashboard

--- screens/DashboardScreen.jsx
+++ screens/DashboardScreen.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { View, Image } from "react-native";
+import { View, Image, ScrollView} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import moment from "moment";
 import Icon from "react-native-vector-icons/Ionicons";
 import ContainerComponent from "../components/ContainerComponent";
 import MyFlatList from "../components/utility/MyFlatList";
-import { H3, H4, H5, P } from "../components/text";
+import { H2,H3, H4, H5, P } from "../components/text";
 import CardFullWidth from "../components/card/CardFullWidth";
 import StatCard from "../components/card/Statcard";
+
 import {
   layouts,
   LIGHT,
@@ -51,22 +52,100 @@ export default function DashboardScreen() {
         />
       </View>
       {/* Welcome message */}
-     
-
-      <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18, }]}>
-        <H3 style={typography.textBold}>My Attendance</H3>
+   <ScrollView >
+      <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18 }]}>
         <CardFullWidth backgroundColor={LIGHT}>
-          <View style={[styles.row, { alignItems: 'center' }]}>
+          <View style={[styles.row, { alignItems: "center" }]}>
             <Icon name="alarm" size={64} color={PRIMARY_COLOR} />
             <TouchableOpacity style={layouts.center}>
               <Icon name="log-out-outline" size={32} color={PRIMARY_COLOR} />
               <H4>Clock Out</H4>
             </TouchableOpacity>
           </View>
-          <H5>Clock started at: {moment().format('HH:mm:ss A')}</H5>
+          <H5>Clock started at: {moment().format("HH:mm:ss A")}</H5>
         </CardFullWidth>
       </View>
-      
+    
+  
+        <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18 }]}>
+          <CardFullWidth backgroundColor={LIGHT}>
+            <View style={[styles.row, { alignItems: "center" }]}>
+              <Icon name="document-text" size={64} color={PRIMARY_COLOR} />
+              <TouchableOpacity style={layouts.center}>
+                <H2>0</H2>
+                <H5>My Open Tasks</H5>
+              </TouchableOpacity>
+            </View>
+          </CardFullWidth>
+        </View>
+        <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18 }]}>
+          <CardFullWidth backgroundColor={LIGHT}>
+            <View style={[styles.row, spacing.mr5, { alignItems: "center" }]}>
+              <Icon name="calendar-clear" size={34} color={PRIMARY_COLOR} />
+              <H5 style={[typography.textBold, { marginRight: 150 }]}>
+                Project Overview
+              </H5>
+            </View>
+            <View style={[spacing.bbw05, spacing.mv1]} />
+            <View
+              style={[
+                styles.row,
+                { justifyContent: "space-between", paddingVertical: 10 },
+              ]}
+            >
+              <View style={{ alignItems: "center" }}>
+                <P style={typography.textBold}>Open</P>
+                <P>0</P>
+              </View>
+              <View style={{ alignItems: "center" }}>
+                <P style={typography.textBold}>Completed</P>
+                <P>0</P>
+              </View>
+              <View style={{ alignItems: "center" }}>
+                <P style={typography.textBold}>Hold</P>
+                <P>0</P>
+              </View>
+            </View>
+            <TouchableOpacity style={layouts.center}></TouchableOpacity>
+          </CardFullWidth>
+        </View>
+        <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18 }]}>
+          <CardFullWidth backgroundColor={LIGHT}>
+            <View style={[styles.row, spacing.mr5, { alignItems: "center" }]}>
+              <Icon name="filter" size={34} color={PRIMARY_COLOR} />
+              <H5 style={[typography.textBold, { marginRight: 150 }]}>
+                All Tasks Overview
+              </H5>
+            </View>
+            <View style={[spacing.bbw05, spacing.mv1]} />
+            <View
+              style={[
+                styles.row,
+                { justifyContent: "space-between", paddingVertical: 10 },
+              ]}
+            ></View>
+            <TouchableOpacity style={layouts.center}></TouchableOpacity>
+          </CardFullWidth>
+        </View>
+        <View style={[spacing.mt2, { width: SCREEN_WIDTH - 18 }]}>
+          <CardFullWidth backgroundColor={LIGHT}>
+            <View style={[styles.row, spacing.mr5, { alignItems: "center" }]}>
+              <Icon name="document-text" size={34} color={PRIMARY_COLOR} />
+              <H5 style={[typography.textBold, { marginRight: 150 }]}>
+                My timeSheet
+              </H5>
+            </View>
+            <View style={[spacing.bbw05, spacing.mv1]} />
+            <View
+              style={[
+                styles.row,
+                { justifyContent: "space-between", paddingVertical: 10 },
+              ]}
+            ></View>
+            <TouchableOpacity style={layouts.center}></TouchableOpacity>
+          </CardFullWidth>
+        </View>
+      </ScrollView>
 
       {/* Attendance Data */}
 
