7e9d077651be1abb7501b526baba921cfaac986d
Bittu75469 <bittukr4715@gmail.com>
2024-10-28 12:50:50 +0530

    message

--- App.js
+++ App.js
@@ -5,13 +5,6 @@ import MyNavigationContainer from "./navigation/MyNavigationContainer";
 import { checkPermissions } from "./utils/checkPermissions";
 
 export default function App() {
-<<<<<<< HEAD
-  useEffect(() => {
-    checkPermissions();
-  }, []);
-=======
-
->>>>>>> 229a364630b49354edd39a8b94e196186375964d
   return (
     <PaperProvider>
       <MyNavigationContainer />
