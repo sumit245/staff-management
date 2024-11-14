1a6fe5208017c3734b564671628b17eeee767860
Rohit <rgupta6205@gmail.com>
2024-11-14 15:03:03 +0530

    create screen

--- components/MyImageBackground.jsx
+++ components/MyImageBackground.jsx
@@ -1,19 +1,38 @@
-import { Image, View } from 'react-native'
-import ContainerComponent from './ContainerComponent'
-import { layouts, spacing, styles } from '../styles'
+// import { Image, View } from 'react-native'
+// import ContainerComponent from './ContainerComponent'
+// import { layouts, spacing, styles } from '../styles'
 
-export default function MyImageBackground({ children }) {
-    return (
-        <ContainerComponent>
-            <Image
-                source={require('../assets/Login.png')}
-                style={[styles.imageContainerImg, spacing.mt5]}
-                resizeMode='contain'
-            />
+// export default function MyImageBackground({ children }) {
+//     return (
+//         <ContainerComponent>
+//             <Image
+//                 source={require('../assets/Login.png')}
+//                 style={[styles.imageContainerImg, spacing.mt5]}
+//                 resizeMode='contain'
+//             />
 
-            <View style={layouts.flex1}>
-                {children}
-            </View>
-        </ContainerComponent>
-    )
+//             <View style={layouts.flex1}>
+//                 {children}
+//             </View>
+//         </ContainerComponent>
+//     )
+// }
+
+import React from "react";
+import { Image, View } from "react-native";
+import ContainerComponent from "./ContainerComponent";
+import { layouts, spacing, styles } from "../styles";
+
+export default function MyImageBackground({ children, imageSource }) {
+  return (
+    <ContainerComponent>
+      <Image
+        source={imageSource}
+        style={[styles.imageContainerImg, spacing.mt5]}
+        resizeMode="contain"
+      />
+
+      <View style={layouts.flex1}>{children}</View>
+    </ContainerComponent>
+  );
 }
