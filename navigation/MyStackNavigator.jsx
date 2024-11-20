8ca7c66d72dd617d8c7df0c7c3feb725c2774092
Rohit <rgupta6205@gmail.com>
2024-11-20 10:53:25 +0530

    write logic in  reducer and action

--- navigation/MyStackNavigator.jsx
+++ navigation/MyStackNavigator.jsx
@@ -37,7 +37,7 @@ const Stack = createNativeStackNavigator();
 export default function MyStackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="ToDoTaskCardScreen"
+      initialRouteName="loginScreen"
       screenOptions={{
         headerShown: false,
       }}
